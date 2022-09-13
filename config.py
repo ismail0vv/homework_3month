7e1f906fe5b58cd3e4c84fb0c523f991e93f0f18
asylbek <asylismailov05@gmail.com>
2022-09-13 20:40:04 +0600

    homework 2

--- config.py
+++ config.py
@@ -2,5 +2,7 @@ from aiogram import Bot, Dispatcher
 from decouple import config
 TOKEN = config("TOKEN")
 
+
+ADMINS = ['5022825338']
 bot = Bot(TOKEN)
 dp = Dispatcher(bot=bot)
--- main.py
+++ main.py
@@ -1,85 +1,13 @@
-import random
-from aiogram import types
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils import executor
 from config import bot, dp
 import logging
+from handlers import admin, callback, extra, client
 
+client.register_handlers_client(dp)
+callback.register_handlers_callback(dp)
+admin.register_handlers_admin(dp)
 
-@dp.message_handler(commands=['start'])
-async def start_command(message: types.Message):
-    await message.answer(f'Жакшысынарбы {message.from_user.full_name}')
-
-
-@dp.message_handler(commands=['mem'])
-async def mem_command(message: types.Message):
-    pornhub = [
-        "media/mem1.jpg",
-        "media/mem2.jpg",
-        "media/mem3.jpg"
-    ]
-    photo = open(random.choice(pornhub), "rb")
-    await bot.send_photo(message.chat.id, photo=photo)
-    photo.close()
-
-
-@dp.message_handler(commands=["quiz"])
-async def quiz_1(message: types.Message):
-    markup = InlineKeyboardMarkup()
-    button_call_1 = InlineKeyboardButton("След.", callback_data='button_call_1')
-    markup.add(button_call_1)
-
-    question = "Кто придумал C++"
-    answers = [
-        'Бог',
-        'Никлаус Вирт',
-        'Нашальникаа',
-        'Бьёрн Страуструп',
-        'Никлаус Вирт',
-    ]
-    await bot.send_poll(
-        chat_id=message.chat.id,
-        question=question,
-        options=answers,
-        is_anonymous=False,
-        type='quiz',
-        correct_option_id=3,
-        explanation="ЕГО ПРИДУМАЛ САТАНА",
-        open_period=30,
-        reply_markup=markup
-    )
-
-
-@dp.callback_query_handler(lambda call: call.data == "button_call_1")
-async def quiz_2(call: types.CallbackQuery):
-    question = "Сколько будет 5+7?"
-    answers = [
-        "Одиннадцать",
-        "Адиннадцать",
-        "Я хз вообще"
-    ]
-
-    await bot.send_poll(
-        chat_id=call.message.chat.id,
-        question=question,
-        options=answers,
-        is_anonymous=False,
-        type='quiz',
-        correct_option_id=2,
-        explanation="Будет двеннадцать",
-        open_period=10
-    )
-
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    if message.text.isnumeric():
-        if len(message.text) > 3:
-            await message.answer('я не буду это умножать мне лень')
-        else:
-            await message.answer(int(message.text) ** 2)
-    else:
-        await bot.send_message(message.from_user.id, message.text)
+extra.register_handlers_extra(dp)
 
 
 if __name__ == "__main__":
